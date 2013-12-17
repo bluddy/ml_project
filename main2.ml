5cfe9a91e6ae2124e2fd658cefd2d7db373ca9b0
Yotam Barnoy <yotambarnoy@gmail.com>
2013-12-17 15:28:31 -0500

    Stop when converged

--- main2.ml
+++ main2.ml
@@ -159,17 +159,18 @@ let gradient_sweep p ffs =
   (*Printf.eprintf "\n";*)
   ffs'
  
-let gradient_ascent p ffs =
-  let rec loop ffs = function
+let gradient_ascent p ffs ll =
+  let rec loop ffs last_ll = function
     | 0 -> ffs
     | i ->
       let newffs = gradient_sweep p ffs in
       let ll = calculate_likelihood p.input_file p.label_file newffs p.window
         p.num_states p.num_atoms (p.queries, p.clique_tree) p.sigma_squared
       in
+      if ll -. last_ll = 0. then ffs else begin
       print_endline @: sof ll;
-      loop newffs (i-1)
-  in loop ffs p.num_iter
+      loop newffs ll (i-1) end
+  in loop ffs ll p.num_iter 
 
 let params = {
   input_file = "";
@@ -236,14 +237,14 @@ let main () =
     let label_file = params.label_file in
     if label_file = "" then print_endline usage_msg else
     (* build feature functions *)
-    let ffs = (* build_1state_xffs num_states num_atoms 
+    let ffs =  build_1state_xffs num_states num_atoms 
              @ build_1state_xffs2 num_states num_atoms 
-             @ *) build_transition_ffs num_states
+             @ build_transition_ffs num_states
     in
     let ll = calculate_likelihood obs_file label_file ffs window
       num_states num_atoms (p.queries, p.clique_tree) p.sigma_squared
     in print_endline @: sof ll;
-    let newffs = gradient_ascent params ffs in
+    let newffs = gradient_ascent params ffs ll in
     print_ffs newffs
 
   | GenLabels ->
